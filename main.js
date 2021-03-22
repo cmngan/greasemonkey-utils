40e8c5c2604ad5155852f5cd1a075c689882f70c
CM Ngan <plusonecm@gmail.com>
2021-03-22 16:34:58 +0800

    Add more fn

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ const getAllElementByTag = (tag, field, value, isInclude) =>
   Array.from(document.getElementsByTagName(tag)).filter((a) =>
     isInclude ? a[field].includes(value) : a[field] === value
   );
+
+const delay = (sec) => new Promise((res) => setTimeout(res, sec));
+const waitFor = async (checkFn) => {
+  let count = 0;
+  while (!checkFn()) {
+    if (count > 100) {
+      console.error("wait too long");
+      throw new Error("wait too long");
+    }
+    count++;
+    // console.log(`try again after 100ms, time: ${count}`)
+    await delay(100);
+  }
+};
